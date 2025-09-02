50a386fa5ae08803cdc90c410acb380b9412f991
Andy Li <andyli26>
2025-09-02 21:28:22 +1000

    update experiment CMD for human file

--- exp/genCMD.py
+++ exp/genCMD.py
@@ -9,6 +9,7 @@ for m in ["empty-8-8", "empty-16-16", "random-32-32-10", "warehouse-10-20-10-2-1
     for i_type in ["random","even"]:
         for i in range(1,26):
             fn_ins=f"{fn_folder}{m}/{i_type}-{i}.scen"
+            fn_human=f"{fn_folder}{m}/human-0.scen"
             with open(fn_ins) as f:
                 maxlen=len(f.read().strip().split("\n"))-1
 
@@ -17,7 +18,7 @@ for m in ["empty-8-8", "empty-16-16", "random-32-32-10", "warehouse-10-20-10-2-1
                     for merge_method in ["stop", "MCP","superMCP", "Sub-OPTIMAL-P1","Sub-OPTIMAL","OPTIMAL"]:
                         fn_opt=fn_opt_temp+f"{BIGI}"+".opt"
                         BIGI+=1
-                        cmd=[exe,"-m",fn_m,"-a",fn_ins, "-r",r,"-t",60,
+                        cmd=[exe,"-m",fn_m,"-a",fn_ins,"-h", fn_human, "-r", r, "-t",60,
                              "--initAlgo",init_method,"--mergeAlgo",merge_method,
                              "--stats",fn_opt]
                         all.append(cmd)
